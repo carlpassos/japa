8cf9770eafd930443d78a066e7cfadf101cd99f3
Carlos Passos <carlospassos93@gmail.com>
2021-08-03 22:52:52 -0300

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1>Bem vindo ao Next Japa</h1>
+  return (
+    <div>
+      <h1>Bem vindo ao next japa</h1>
+      <h3>com deploy automatico</h3>
+    </div>
+    )
 }
